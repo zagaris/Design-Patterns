639d8122480f2586b64f3cc5f6a54ca39bb2b7c3
ploukareas <ploukareas@gmail.com>
2021-07-15 01:06:21 +0300

    🔨 memento pattern

---
+++ Python/behavioral_patterns/memento/main.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from time import sleep
+from behavioral_patterns.memento.gamer import Gamer
+
+'''
+A dice game in which money increases and decreases:
+* A gamer shakes a dice and the number determine the next state.
+* If the number of dice is even, gamer's money doubles, and if it is odd, gamer's money is halved.
+* If the gamer's money is less than half of the highest amount, it returns to the highest amount.
+* The game is repeated.
+'''
+
+if __name__ == '__main__':
+    gamer = Gamer(100)                      # The initial money is 100
+    memento = gamer.create_memento()        # Save the initial state
+
+    for i in range(0, 10):
+        print('==== Turn ' + str(i + 1))    # Display count
+
+        gamer.play()                        # Play a game
+
+        # Determine the behavior of the Memento
+        if gamer.money > memento.money:
+            print('(Gamers\' money is the highest ever, so record the current state.)')
+            memento = gamer.create_memento()
+        elif gamer.money < memento.money / 2:
+            print('(Gamer\'s money is less than half of the highest amount, so return to the recorded state.)')
+            gamer.set_memento(memento)
+            print('Gamer\'s money returns to ' + str(gamer.money) + '.')
+
+        print('')
+
+        sleep(1)
+
---
+++ Python/behavioral_patterns/memento/memento.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# ˅
+
+# ˄
+
+
+class Memento(object):
+    # ˅
+    
+    # ˄
+
+    def __init__(self, money):
+
+        # Money
+        self.__money = money
+
+        # ˅
+        pass
+        # ˄
+
+    @property
+    def money(self):
+        # ˅
+        return self.__money
+        # ˄
+
+    # ˅
+    
+    # ˄
+
+
+# ˅
+
+# ˄
